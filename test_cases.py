026d1d16a6e7ccd9fbe480c78046b070e54a268f
Erin Thomas <eethomas@bu.edu>
2020-01-29 14:09:00 -0500

    Fixed pytest error by changing naming conventions

--- test_cases.py
+++ test_cases.py
@@ -3,7 +3,7 @@
 import pytest
 from arabic2roman import arabic2roman
 
-def test():
+def test_good():
     assert arabic2roman(3789) == 'MMMDCCLXXXIX'
     assert arabic2roman(7) == 'VII'
     assert arabic2roman(67) == 'LXVII'
@@ -12,4 +12,4 @@ def test():
     assert arabic2roman(3999) == 'MMMCMXCIX'
 
 if __name__ == "__main__":
-    test()
+    test_good()
