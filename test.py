f14c1f90139afcb9af1b575dcacfddbf59842775
Erin Thomas <eethomas@bu.edu>
2020-01-29 13:21:53 -0500

    Added test cases

--- test.py
+++ test.py
@@ -3,5 +3,13 @@
 from arabic2roman import arabic2roman
 import pytest
 
+def test():
+    assert arabic2roman(3789) == "MMMDCCLXXXIX"
+    assert arabic2roman(7) == "VII"
+    assert arabic2roman(67) == "LXVII"
+    assert arabic2roman(555) == "DLV"
+    assert arabic2roman(1) == "I"
+    assert arabic2roman(3999) == "MMMCMXCIX"
+
 if __name__ == "__main__":
-    # function test()
+    test()
