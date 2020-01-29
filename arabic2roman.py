aed02db20bf6d04ec5c99ad11ad4d007d1fb4bb2
Erin Thomas <eethomas@bu.edu>
2020-01-29 10:57:38 -0500

    Algorithm to convert arabic integers to roman numerals

--- arabic2roman.py
+++ arabic2roman.py
@@ -1,4 +1,22 @@
+# Erin Thomas / U26838058 / EC 500: Building Software / Assignment 1
 # Program to convert Arabic numbers to Roman numerals
+# Reference: https://www.geeksforgeeks.org/converting-decimal-number-lying-between-1-to-3999-to-roman-numerals/
 
-def arabic2roman():
-    arabic_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+def arabic2roman(number):
+    arabic_num = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+    roman_symb = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
+
+    i = len(arabic_num)
+    while number:
+        # Repeats until quotient is zero
+        quot = number // arabic_num[i]
+        number %= arabic_num[i]
+
+        # Prints roman numerals equivalent to quotient (e.g. 2300/1000 --> quot = 2, so M will print twice )
+        while quot: 
+            print(roman_symb[i], roman_num = "")
+            quot -= 1
+        i -= 1
+    return roman_num
+
+
